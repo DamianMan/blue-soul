d245107ff94b8cb78251e8bd412c20893cdd516a
Damiano Manzillo <damianomanzillo@Damiano-MacBook.local>
2024-11-02 21:08:01 +0100

    Updated app.json to app.config.js

---
+++ app.config.js
@@ -0,0 +1,55 @@
+export default {
+  expo: {
+    name: "BlueSoul",
+    slug: "blue-soul-app",
+    scheme: "BlueSoul-App",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/logo.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/logo.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.blueSoul.firebaseApp",
+      googleServicesFile: "./GoogleService-Info.plist",
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/logo.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.blueSoul.firebaseApp",
+      versionCode: 1,
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+    },
+    web: {
+      favicon: "./assets/logo.png",
+      web: {
+        bundler: "metro",
+      },
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+          },
+        },
+      ],
+      "@react-native-firebase/app",
+      "@react-native-firebase/auth",
+    ],
+    extra: {
+      eas: {
+        projectId: "720f0423-e117-420e-b7d6-e7be3a11f842",
+      },
+    },
+    owner: "damianomanzillo",
+  },
+};
